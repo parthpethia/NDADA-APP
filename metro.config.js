4a62c342cef8bc565bc202c90daaff6f0e30f658
Parth Pethia <parthpethia@gmail.com>
2026-06-03 06:22:27 +0530

    fix(web): alias tslib to ES6 version to fix __extends runtime error

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,18 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Force Metro to resolve tslib to the ES6 version to prevent __extends of undefined error on web
+const ALIASES = {
+  tslib: 'tslib/tslib.es6.js',
+};
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  return context.resolveRequest(
+    context,
+    ALIASES[moduleName] ?? moduleName,
+    platform
+  );
+};
+
 module.exports = withNativeWind(config, { input: './global.css' });
+
